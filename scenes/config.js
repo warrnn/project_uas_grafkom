ec7dc2f6d6c57bca46b26859d06defc3a26315fd
Bryan Warren <bryanwarren86@gmail.com>
2025-11-17 15:08:23 +0700

    uncomment scene config after making scene 3

--- scenes/config.js
+++ scenes/config.js
@@ -3,7 +3,7 @@ import { loadAnimationScene2, loadScene2 } from "./scene2";
 import { loadAnimationScene3, loadScene3 } from "./scene3";
 
 export const SCENE_LIST = {
-//     1: { load: loadScene1, animate: loadAnimationScene1 },
-//     2: { load: loadScene2, animate: loadAnimationScene2 },
+    1: { load: loadScene1, animate: loadAnimationScene1 },
+    2: { load: loadScene2, animate: loadAnimationScene2 },
     1: { load: loadScene3, animate: loadAnimationScene3 },
 };
