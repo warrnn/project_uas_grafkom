99b3777f4dc85b5ffe7957fae24ff65901acc815
Bryan Warren <bryanwarren86@gmail.com>
2025-11-17 16:16:10 +0700

    fixing typo

--- scenes/config.js
+++ scenes/config.js
@@ -5,5 +5,5 @@ import { loadAnimationScene3, loadScene3 } from "./scene3";
 export const SCENE_LIST = {
     1: { load: loadScene1, animate: loadAnimationScene1 },
     2: { load: loadScene2, animate: loadAnimationScene2 },
-    1: { load: loadScene3, animate: loadAnimationScene3 },
+    3: { load: loadScene3, animate: loadAnimationScene3 },
 };
