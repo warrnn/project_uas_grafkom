2446daa30ac3066bf45ad6f11d7465aa59ffc734
Bryan Warren <bryanwarren86@gmail.com>
2025-11-18 14:13:29 +0700

    scene 7 and scene 8 environment done

--- scenes/config.js
+++ scenes/config.js
@@ -4,6 +4,8 @@ import { loadAnimationScene3, loadScene3 } from "./scene3";
 import { loadAnimationScene4, loadScene4 } from "./scene4";
 import { loadAnimationScene5, loadScene5 } from "./scene5";
 import { loadAnimationScene6, loadScene6 } from "./scene6";
+import { loadAnimationScene7, loadScene7 } from "./scene7";
+import { loadAnimationScene8, loadScene8 } from "./scene8";
 
 export const SCENE_LIST = {
     1: { load: loadScene1, animate: loadAnimationScene1 },
@@ -12,4 +14,6 @@ export const SCENE_LIST = {
     4: { load: loadScene4, animate: loadAnimationScene4 },
     5: { load: loadScene5, animate: loadAnimationScene5 },
     6: { load: loadScene6, animate: loadAnimationScene6 },
+    7: { load: loadScene7, animate: loadAnimationScene7 },
+    8: { load: loadScene8, animate: loadAnimationScene8 },
 };
---
+++ scenes/scene8.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { enableShadows, onError } from '../helpers/functionHelper';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { ColorGUIHelper } from '../helpers/classHelper';
+import { degToRad } from 'three/src/math/MathUtils.js';
+
+export function loadScene8(scene, models, mixers) {
+    const loader = new GLTFLoader();
+    const gui = new GUI();
+
+    /* Background */
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.load('/backgrounds/bg_scene_6.jpg', (texture) => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        scene.background = texture;
+    });
+
+    /* Lights */
+    const ambientLight = new THREE.AmbientLight("#ffda24", 1);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    directionalLight.position.set(-100, 13.9, 79.1);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+
+    directionalLight.shadow.camera.near = -500;
+    directionalLight.shadow.camera.far = 500;
+    directionalLight.shadow.camera.left = -500;
+    directionalLight.shadow.camera.right = 500;
+    directionalLight.shadow.camera.top = 500;
+    directionalLight.shadow.camera.bottom = -500;
+
+    const dirHelper = new THREE.DirectionalLightHelper(directionalLight);
+    const shadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+    // scene.add(dirHelper, shadowHelper);
+
+    scene.userData.directionalLightHelper = dirHelper;
+    scene.userData.shadowHelper = shadowHelper;
+
+    const lightFolder = gui.addFolder("Lights");
+
+    const ambientFolder = lightFolder.addFolder("Ambient Light");
+    ambientFolder.addColor(new ColorGUIHelper(ambientLight, 'color'), 'value');
+    ambientFolder.add(ambientLight, "intensity", 0, 5, 0.01);
+
+    const dirFolder = lightFolder.addFolder("Directional Light");
+    dirFolder.addColor(new ColorGUIHelper(directionalLight, 'color'), 'value');
+    dirFolder.add(directionalLight, "intensity", 0, 10, 0.1);
+    dirFolder.add(directionalLight.position, "x", -100, 100, 0.1);
+    dirFolder.add(directionalLight.position, "y", -100, 100, 0.1);
+    dirFolder.add(directionalLight.position, "z", -100, 100, 0.1);
+
+    /* 3D Object Loads */
+    loader.load('/environment/flatiron_building.glb', (gltf) => {
+        const model = gltf.scene;
+        model.scale.set(75, 75, 75);
+        model.position.set(0, 0, 0);
+        enableShadows(model);
+        scene.add(model);
+    }, undefined, onError);
+}
+
+export function loadAnimationScene8(models, scene) {
+
+}
